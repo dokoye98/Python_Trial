8c0de81db042d5573c152a0f04f617290da3c773
Dokoye98 <dwayneokoye98@gmail.com>
2023-05-14 14:57:32 +0100

    String 1

---
+++ BasicTraining/Strings.py
@@ -0,0 +1,20 @@
+#splitting
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(",")
+print(author_names)
+a = []
+b = []
+both_names = [i.split() for i in author_names]
+print(both_names)
+first_names = [i.split()[0] for i in author_names] 
+print(first_names)
+last_names = [i.split()[-1] for i in author_names]
+print(last_names)
+#joining
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+reapers_line_one = " ".join(reapers_line_one_words)
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+winter_trees_full = "\n".join(winter_trees_lines)
+print(winter_trees_full)
